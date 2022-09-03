87dd1ff81455c6067dedce6c09b2aa734c4c3d66
Adam Raaif Nasheed <43497786+Remls@users.noreply.github.com>
2022-09-03 06:09:08 +0500

    Add rarity counts

--- chars.py
+++ chars.py
@@ -3,6 +3,7 @@ import csv, urllib.parse
 GENSHIN_WIKI = "https://genshin-impact.fandom.com/wiki/"
 WEAPONS = ["Bow", "Catalyst", "Claymore", "Polearm", "Sword", "Unknown"]
 ELEMENTS = ["Anemo", "Geo", "Electro", "Dendro", "Hydro", "Pyro", "Cryo", "Unknown"]
+RARITIES = ["5", "4", "Unknown"]
 
 
 def get_title_with_image(key: str, ext = "svg") -> str:
@@ -18,23 +19,31 @@ def get_wiki_link_to_char(char_name: str, display_name: str) -> str:
     return f"<a href=\"{link}\">{display_name}</a>"
 
 
-# Create 2D dict
-data = {}
+table_data = {}
 for w in WEAPONS:
-    data[w] = {}
+    table_data[w] = {}
     for e in ELEMENTS:
-        data[w][e] = [];
+        table_data[w][e] = [];
+
+rarity_data = {}
+for r in RARITIES:
+    rarity_data[r] = {}
+    for w in WEAPONS:
+        rarity_data[r][w] = 0;
+    for e in ELEMENTS:
+        rarity_data[r][e] = 0;
 
 with open('chars.csv', newline='') as f:
     reader = csv.DictReader(f)
     for row in reader:
-        w = row['weapon']
-        e = row['element']
-        if not w:
-            w = "Unknown"
-        if not e:
-            e = "Unknown"
-        data[w][e].append(get_wiki_link_to_char(row['name'], row['display_name']))
+        w = row['weapon']  or "Unknown"
+        e = row['element'] or "Unknown"
+        table_data[w][e].append(get_wiki_link_to_char(row['name'], row['display_name']))
+        for r in RARITIES:
+            if row['rarity'] == r:
+                rarity_data[r][w] += 1
+                rarity_data[r][e] += 1
+                break
 
 # Build output
 output = """
@@ -52,17 +61,22 @@ output = """
 headers = []
 for w in WEAPONS:
     headers.append(f"<th align=\"center\">{get_title_with_image(w, 'webp')}</th>")
+headers.append(f"<th align=\"center\"><b>RARITY</b></th>")
 output = output.replace("[HEADERS]", "\n".join(headers))
 
 table = []
 for e in ELEMENTS:
     line = f"<tr><td align=\"center\">{get_title_with_image(e)}</td>"
     for w in WEAPONS:
-        line += f"<td>{',<br>'.join(data[w][e])}</td>"
+        line += f"<td>{',<br>'.join(table_data[w][e])}</td>"
+    rarity_display = []
+    for r in RARITIES:
+        r = f"{r}✭" if r != "Unknown" else r
+        rarity_display.append(f"{r} {rarity_data[r][e]}")
+    line += f"<td>{'<br>'.join(rarity_display)}</td>"
     line += "</tr>"
     table.append(line)
 output = output.replace("[TABLE]", "\n".join(table))
-    
 
 with open("README.md", "w") as f:
     f.write(output)
